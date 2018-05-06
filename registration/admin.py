a957c88db950874ad2596c8995b9b86c869cec1e
Xavier Janer <xavier.janer@qustodio.com>
2018-05-06 19:13:08 +0200

    + Skip the interactive activation step. + Replace the activate_users action with approve_users.

--- registration/admin.py
+++ registration/admin.py
@@ -2,28 +2,38 @@ from django.contrib import admin
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.translation import ugettext_lazy as _
 
+from .backends.admin_approval.views import SupervisedRegistrationProfile
 from .models import RegistrationProfile
 from .users import UsernameField
 
 
 class RegistrationAdmin(admin.ModelAdmin):
-    actions = ['activate_users', 'resend_activation_email']
-    list_display = ('user', 'activation_key_expired')
+    actions = ['approve_users', 'resend_activation_email']
+    list_display = ('user', 'get_name', 'get_is_active')
     raw_id_fields = ['user']
     search_fields = ('user__{0}'.format(UsernameField()),
                      'user__first_name', 'user__last_name')
 
-    def activate_users(self, request, queryset):
+    def get_name(self, obj):
+        return obj.user.first_name
+    get_name.admin_order_field = 'name'  # Allows column order sorting
+    get_name.short_description = _('Name')  # Renames column head
+
+    def get_is_active(self, obj):
+        return obj.user.is_active
+    get_is_active.admin_order_field = 'is_active'  # Allows column order sorting
+    get_is_active.short_description = _('Is Active?')  # Renames column head
+
+    def approve_users(self, request, queryset):
         """
-        Activates the selected users, if they are not already
-        activated.
+        Approves the selected users.
 
         """
 
         site = get_current_site(request)
         for profile in queryset:
-            RegistrationProfile.objects.activate_user(profile.activation_key, site)
-    activate_users.short_description = _("Activate users")
+            SupervisedRegistrationProfile.objects.admin_approve_user(profile.id, site)
+    approve_users.short_description = _("Approve users")
 
     def resend_activation_email(self, request, queryset):
         """
@@ -43,5 +53,10 @@ class RegistrationAdmin(admin.ModelAdmin):
 
     resend_activation_email.short_description = _("Re-send activation emails")
 
+    def get_actions(self, request):
+        actions = super(RegistrationAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
 
 admin.site.register(RegistrationProfile, RegistrationAdmin)
